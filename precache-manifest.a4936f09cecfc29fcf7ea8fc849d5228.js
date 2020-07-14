c26b67f648321e408c57bc4ef3cff2f97e3a203b
harvey67 <67565077+harvey67@users.noreply.github.com>
2020-07-14 10:57:09 +1000

    Updates

---
+++ precache-manifest.a4936f09cecfc29fcf7ea8fc849d5228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2425b7c53d1553f90f670936329f67",
+    "url": "/nrl-draft/index.html"
+  },
+  {
+    "revision": "330c611c05de25459104",
+    "url": "/nrl-draft/static/css/main.b9408007.chunk.css"
+  },
+  {
+    "revision": "43e4398e9b82f35ec235",
+    "url": "/nrl-draft/static/js/2.b389b59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nrl-draft/static/js/2.b389b59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330c611c05de25459104",
+    "url": "/nrl-draft/static/js/main.392dc381.chunk.js"
+  },
+  {
+    "revision": "b5b9b906f2816745fd81",
+    "url": "/nrl-draft/static/js/runtime-main.70cb2121.js"
+  }
+]);
